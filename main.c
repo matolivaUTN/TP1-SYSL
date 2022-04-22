5d6ac026eba05e7d798a0919b86a97e9047b9a8b
matiasolivaa <olivamatiasd@gmail.com>
2022-04-22 18:09:43 -0300

    tp1

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main()
+{
+    char c;
+    c = getchar();
+    while(c != '\n'){
+        if(isdigit(c)){
+            c = getchar();
+        }
+        else{
+            if(isupper(c)){
+                putchar(tolower(c));
+            }
+            else{
+                putchar(toupper(c));
+            }
+            c = getchar();
+        }
+    }
+
+    return 0;
+}
